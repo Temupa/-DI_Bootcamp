e033be0934ecf2bccea251ab0b10e18bfc7304f7
Temupa <safeitai@gmail.com>
2021-08-21 21:20:01 +0300

    Update

---
+++ Week_3/Day_1/XP/script.js
@@ -0,0 +1,15 @@
+
+// 5Exrcise 1 :
+// 5 + "34" =534
+// 5 - "4" = 1
+// 10 % 5 = 2
+// 5 % 10 = 0
+// "Java" + "Script" ="javascript"
+// " " + " " = "   "
+// " " + 0  = " 0"
+// true + true = 2
+// true + false= 1
+// false + true= 1
+// false - true= -1
+// 3 - 4=  -1
+// "Bob" - "bill" =NaN
